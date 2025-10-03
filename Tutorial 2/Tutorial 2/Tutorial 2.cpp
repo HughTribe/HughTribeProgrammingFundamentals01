d660360334fc4aafadb4a6802e2cbde6ce01477e
HughTribe <229113637+HughTribe@users.noreply.github.com>
2025-10-03 16:50:42 +0100

    tutorial 2 stuff

---
+++ Tutorial 2/Tutorial 2/Tutorial 2.cpp
@@ -0,0 +1,40 @@
+// Tutorial 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    std::cout << (9503 * 92) << '\n';
+    int factorial = 5;
+    for (int i = 1; i < 5; i++)
+    {
+        factorial *= i;
+    }
+        
+   std::cout << factorial << '\n';
+
+   for (int i = 1; i < 13; i++)
+   {
+       std::cout << i << " x 6 = " << 6 * i << '\n';
+
+   }
+
+}
+
+
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
