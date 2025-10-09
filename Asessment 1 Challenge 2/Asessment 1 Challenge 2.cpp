cbbdfe4f21d68a753bab50e20b3f60110c5923f9
HughTribe <229113637+HughTribe@users.noreply.github.com>
2025-10-09 17:50:53 +0100

    Assessment 1 Challenge 2
    got two errors rn 😭 hopefully someone will know how to help cus im clueless, might ask my dad :)

---
+++ Asessment 1 Challenge 2/Asessment 1 Challenge 2.cpp
@@ -0,0 +1,46 @@
+// Asessment 1 Challenge 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+
+int main() 
+{ 
+    std::string name;
+    std::string username;
+    std::string clantag;
+    int Exp;
+    std::cout << "What is your name?\n";
+    std::cin >> name;
+    std::cout << "What is your username?\n";
+    std::cin >> username;
+    std::cout << "What is your clantag?\n";
+    std::cin >> clantag;
+    std::cout << "How many experience points do you have?\n";
+    std::cin >> Exp;
+    std::cout << "Your name is " << name << " and your in-game name is [ " << clantag << " ]" << username;
+    std::cout << "\nYou have " << Exp << " experience points\n\n";
+    std::cout << "You are currently level " << WhatLevelAndHowClosetoNextLevel(Exp);
+
+        
+
+}
+
+int WhatLevelAndHowClosetoNextLevel (int Exp) {
+
+    int Level = Exp / 100;
+
+
+    return Level;
+
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
