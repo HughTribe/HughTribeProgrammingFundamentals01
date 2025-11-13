99bbefddbeecbc26f17df84e777562e9894e5cbc
HughTribe <229113637+HughTribe@users.noreply.github.com>
2025-11-13 21:08:47 +0000

    commit

--- CardGame!!/CardGame!!/CardGame!!.cpp
+++ CardGame!!/CardGame!!/CardGame!!.cpp
@@ -507,27 +507,38 @@ using namespace std;
  //    string b;
  //};
 //string ArrayOfGraphics[] = {Rogue, WizardCat, JackLloyd};
-
-
+int Turn = 1;
+int Round = 1;
 class player
 { public:
+
     string name;
-    int health = 100;
-    int mana = 0;
-    int strength = 0;
+    int health;
+    int mana;
+    int strength;
     int agility;
-	int armour = 0;
+	int armour;
+
+    player(string n, int h, int m, int s, int a, int ar) : name(n), health(h), mana(m), strength(s), agility(a), armour(ar) {}
+     int pTakeDamage(int damage)
+     {
+         
+        health -= damage;
+        return health;
+       
+	 };
 };
+
+player Player("Player", 100, 5, 0, 0, 0);
 
 class enemy
 {
   public:
-      enemy()
-      {
-                            
-      }
-      string name = "Goblin";
-      int health = 30;
+    
+      string name;
+      int health;
+      enemy(string n, int h) : name(n), health(h) {}
+
      int eTakeDamage(int damage)
      {
          
@@ -536,35 +547,83 @@ class enemy
        
     };
   
-    
-
-
-
 };
 
-player Player;
-enemy Goblin;
+enemy Goblin("Goblin", 30);
 
-bool IsDead(player pHealth, enemy eHealth)
+bool IsDead(player& Player, enemy& Goblin, int& Round, int& Turn)
 {
-    if (pHealth.health <= 0)
+    if (Player.health <= 0)
     {
         system("cls");
-        cout << "YOU DIED" << endl;
-		_getch();
+        cout << "\n\n\n\n\t\t\tYOU DIED\n\t\t\t\t";
+        cout << R"(
+                       uuuuuuuuuuuuuuuuuuuuu.
+                   .u$$$$$$$$$$$$$$$$$$$$$$$$$$W.
+                 u$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Wu.
+               $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+              $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+         `    $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+           .i$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+           $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$W
+          .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$W
+         .$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$i
+         #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$.
+         W$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$u       #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$~
+$#      `"$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$i        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$        #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+$$         $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#$.        $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+ $$      $iW$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$!
+ $$i      $$$$$$$#"" `"""#$$$$$$$$$$$$$$$$$#""""""#$$$$$$$$$$$$$$$W
+ #$$W    `$$$#"            "       !$$$$$`           `"#$$$$$$$$$$#
+  $$$     ``                 ! !iuW$$$$$                 #$$$$$$$#
+  #$$    $u                  $   $$$$$$$                  $$$$$$$~
+   "#    #$$i.               #   $$$$$$$.                 `$$$$$$
+          $$$$$i.                """#$$$$i.               .$$$$#
+          $$$$$$$$!         .   `    $$$$$$$$$i           $$$$$
+          `$$$$$  $iWW   .uW`        #$$$$$$$$$W.       .$$$$$$#
+            "#$$$$$$$$$$$$#`          $$$$$$$$$$$iWiuuuW$$$$$$$$W
+               !#""    ""             `$$$$$$$##$$$$$$$$$$$$$$$$
+          i$$$$    .                   !$$$$$$ .$$$$$$$$$$$$$$$#
+         $$$$$$$$$$`                    $$$$$$$$$Wi$$$$$$#"#$$`
+         #$$$$$$$$$W.                   $$$$$$$$$$$#   ``
+          `$$$$##$$$$!       i$u.  $. .i$$$$$$$$$#""
+             "     `#W       $$$$$$$$$$$$$$$$$$$`      u$#
+                            W$$$$$$$$$$$$$$$$$$      $$$$W
+                            $$`!$$$##$$$$``$$$$      $$$$!
+                           i$" $$$$  $$#"`  """     W$$$$
+                                                   W$$$$!
+                      uW$$  uu  uu.  $$$  $$$Wu#   $$$$$$
+                     ~$$$$iu$$iu$$$uW$$! $$$$$$i .W$$$$$$
+             ..  !   "#$$$$$$$$$$##$$$$$$$$$$$$$$$$$$$$#"
+             $$W  $     "#$$$$$$$iW$$$$$$$$$$$$$$$$$$$$$W
+             $#`   `       ""#$$$$$$$$$$$$$$$$$$$$$$$$$$$
+                              !$$$$$$$$$$$$$$$$$$$$$#`
+                              $$$$$$$$$$$$$$$$$$$$$$!
+                            $$$$$$$$$$$$$$$$$$$$$$$`
+                             $$$$$$$$$$$$$$$$$$$$")";
+		Turn = 1;
+		Round = 1;
+		Player.health = 100;
+		Goblin.health = 30;
+        
         return true;
     }
-    if (eHealth.health <= 0)
+    if (Goblin.health <= 0)
     {
         system("cls");
         cout << "YOU OBLITERATED THE ENEMY" << endl;
-		_getch();
+        Goblin.health = 30;
+		Turn = 1;
+        Round++;
+		
         return true;
     }
-    else
-    {
-        return false;
-    }
+    return false;
+    
 }
 class Card {
 public:
@@ -581,7 +640,7 @@ public:
 
 class Hand
 {
- public:
+public:
     Hand(string Character)
     {
         if (Character == "Wizard")
@@ -611,162 +670,194 @@ class Hand
     }
     vector<Card> vHand;
     void PrintHand(vector<Card> vHand)
-	{
-		
+    {
+
         int cSelected = 0;
         while (true)
-        {           
-            system("cls");
-            cout << endl << '\t';
-            for (int i = 0; i < vHand.size(); i++)
+        {
+            if (Turn % 2 == 0)//enemy turn
             {
-                cout << "___________   ";
-            }                              
-            cout << endl << '\t';
-            for (int i = 0; i < vHand.size(); i++)
-            {
-                cout << "|   (" << vHand.at(i).ManaCost <<")   |   ";
-            }
-            cout << endl << '\t';
-            for (int i = 0; i < vHand.size(); i++)
-            {
-                cout << '|';
 
-                int spaces = (9 - vHand.at(i).Name.length()) / 2;
-
-                for (int j = 0; j < spaces; j++)
-                {
-                    cout << " ";
-
-                }
-
-
-                if (i == cSelected)
-                {
-                    cout << GREEN << vHand.at(i).Name << RESET;
-                }
-                else
-                {
-                    cout << vHand.at(i).Name;
-                }
-
-                for (int j = 0; j < spaces; j++)
-                {
-                    cout << " ";
-                }
-                cout << "|   ";
-            }
-            
-            for(int i = 0; i < 3; i++)
-            {
-                cout << "\n\t";
-                for (int j = 0; j < vHand.size(); j++)
-                {
-                    cout << "|         |   ";
-                }
-            }
-            
-            cout << "\n\t";
-            for (int i = 0; i < vHand.size(); i++)
-            {
-                cout << "|_________|   ";
-            }
-            char key = _getch(); // Get user input without pressing Enter
-            if (key == 75) { // Left arrow key
-                cSelected = (cSelected - 1 + vHand.size()) % vHand.size();
-            }
-            else if (key == 77) { // Right arrow key
-                cSelected = (cSelected + 1) % vHand.size();
-            }
-            else if (key == '\r') { // Enter key
                 system("cls");
-                cout << "You played " << vHand.at(cSelected).Name << " dealing " << vHand.at(cSelected).Damage << " damage!" << endl;
-				cout <<  Goblin.name << " has " << Goblin.eTakeDamage(vHand.at(cSelected).Damage) << " health remaining!" << endl;
-				_getch();
-                IsDead(Player, Goblin);
+                cout << "Enemy's Turn! Press any key to continue..." << endl;
+                _getch();
+				system("cls");
+                int enemyDamage = rand() % 10 + 5; // Random damage between 5 and 15
+                cout << "The " << Goblin.name << " attacks you for " << enemyDamage << " damage!" << endl;
+                Player.pTakeDamage(enemyDamage);
+                cout << "You have " << Player.health << " health remaining!" << endl;
+				Turn++;
+                _getch();
+                if(IsDead(Player, Goblin, Round, Turn))
+                {
+					_getch();
                     continue;
+                }
+               
+            }
+			else //player turn
+            {
+                system("cls");
+                cout << endl << '\t';
+                for (int i = 0; i < vHand.size(); i++)
+                {
+                    cout << "___________   ";
+                }
+                cout << endl << '\t';
+                for (int i = 0; i < vHand.size(); i++)
+                {
+                    cout << "|   (" << vHand.at(i).ManaCost << ")   |   ";
+                }
+                cout << endl << '\t';
+                for (int i = 0; i < vHand.size(); i++)
+                {
+                    cout << '|';
+
+                    int spaces = (9 - vHand.at(i).Name.length()) / 2;
+
+                    for (int j = 0; j < spaces; j++)
+                    {
+                        cout << " ";
+
+                    }
+
+
+                    if (i == cSelected)
+                    {
+                        cout << GREEN << vHand.at(i).Name << RESET;
+                    }
+                    else
+                    {
+                        cout << vHand.at(i).Name;
+                    }
+
+                    for (int j = 0; j < spaces; j++)
+                    {
+                        cout << " ";
+                    }
+                    cout << "|   ";
+                }
+
+                for (int i = 0; i < 3; i++)
+                {
+                    cout << "\n\t";
+                    for (int j = 0; j < vHand.size(); j++)
+                    {
+                        cout << "|         |   ";
+                    }
+                }
+
+                cout << "\n\t";
+                for (int i = 0; i < vHand.size(); i++)
+                {
+                    cout << "|_________|   ";
+                }
+                char key = _getch(); // Get user input without pressing Enter
+                if (key == 75) { // Left arrow key
+                    cSelected = (cSelected - 1 + vHand.size()) % vHand.size();
+                }
+                else if (key == 77) { // Right arrow key
+                    cSelected = (cSelected + 1) % vHand.size();
+                }
+                else if (key == '\r') { // Enter key
+                    system("cls");
+                    cout << "You played " << vHand.at(cSelected).Name << " dealing " << vHand.at(cSelected).Damage << " damage!" << endl;
+                    cout << Goblin.name << " has " << Goblin.eTakeDamage(vHand.at(cSelected).Damage) << " health remaining!" << endl;
+					Turn++;
+					_getch();
+                    if (IsDead(Player, Goblin, Round, Turn))
+                    {
+						_getch();
+						continue;
+                    }
+                    
+
+
+                }
+
 
                 /*Hand H(options[selected]);
                 H.PrintHand(H.vHand, 0);*/
 
+
+
+                /*cout << "Name is " << Name << endl;
+                cout << "Mana cost is " << ManaCost << endl;
+                cout << "Damage is " << Damage << endl;*/
             }
 
-            /*cout << "Name is " << Name << endl;
-            cout << "Mana cost is " << ManaCost << endl;
-            cout << "Damage is " << Damage << endl;*/
-        }
 
 
-
+        };
     };
 };
- 
 
-void displayMenu(const string options[], int size, int selected, string extra) 
-{
-    system("cls"); // Clear the console screen
-    cout << extra << '\n';
-    for (int i = 0; i < size; i++) 
+
+    void displayMenu(const string options[], int size, int selected, string extra)
     {
-        if (i == selected) {
-            cout << " > " << options[i] << " < " << endl; // Highlight selected option
-        }
-        else {
-            cout << "   " << options[i] << endl;
-        }
-    }
-}
-
-void characterSelection()
-{
-    const int optionCount = 3;
-    string options[optionCount] = {"Rogue", "Wizard", "Jack LLoyd"};
-    int selected = 0;
-   /* bool ChosenClass = true;*/
-    while (true) 
-    {
-        displayMenu(options, optionCount, selected, "");
-
-        char key = _getch(); // Get user input without pressing Enter
-        if (key == 72) 
-        { // Up arrow key
-            selected = (selected - 1 + optionCount) % optionCount;
-        }
-        else if (key == 80) 
-        { // Down arrow key
-            selected = (selected + 1) % optionCount;
-        }
-        else if (key == '\r') { // Enter key
-            system("cls");
-            Hand H (options[selected]);
-			H.PrintHand(H.vHand);
-
-            //game loop - move?
-            while (true)
-            {
-
+        system("cls"); // Clear the console screen
+        cout << extra << '\n';
+        for (int i = 0; i < size; i++)
+        {
+            if (i == selected) {
+                cout << " > " << options[i] << " < " << endl; // Highlight selected option
             }
-
-
-            /*if (options[selected] == "Exit") break;
-            system("pause");*/
+            else {
+                cout << "   " << options[i] << endl;
+            }
         }
     }
 
+    void characterSelection()
+    {
+        const int optionCount = 3;
+        string options[optionCount] = { "Rogue", "Wizard", "Jack LLoyd" };
+        int selected = 0;
+        /* bool ChosenClass = true;*/
+        while (true)
+        {
+            displayMenu(options, optionCount, selected, "");
+
+            char key = _getch(); // Get user input without pressing Enter
+            if (key == 72)
+            { // Up arrow key
+                selected = (selected - 1 + optionCount) % optionCount;
+            }
+            else if (key == 80)
+            { // Down arrow key
+                selected = (selected + 1) % optionCount;
+            }
+            else if (key == '\r') { // Enter key
+                system("cls");
+                Hand H(options[selected]);
+                H.PrintHand(H.vHand);
+
+                //game loop - move?
+                while (true)
+                {
+
+                }
 
 
-    
-}
+                /*if (options[selected] == "Exit") break;
+                system("pause");*/
+            }
+        }
 
 
-int main() 
-{
 
-    const int optionCount = 4;
-    string options[optionCount] = { "Start Game", "Option 2", "Option 3", "Exit" };
-    int selected = 0;
 
-    string extra = R"( 
+    }
+
+
+    int main()
+    {
+
+        const int optionCount = 4;
+        string options[optionCount] = { "Start Game", "Option 2", "Option 3", "Exit" };
+        int selected = 0;
+
+        string extra = R"( 
  _    _      _                            _         ______          _           
 | |  | |    | |                          | |        | ___ \        | |          
 | |  | | ___| | ___ ___  _ __ ___   ___  | |_ ___   | |_/ /__ _  __| | ___  ___ 
@@ -776,35 +867,37 @@ int main()
                                                                                 
        )";
 
-    while (true) {
-        displayMenu(options, optionCount, selected, extra);
+        while (true) {
+            displayMenu(options, optionCount, selected, extra);
 
-        char key = _getch(); // Get user input without pressing Enter
-        if (key == 72) { // Up arrow key
-            selected = (selected - 1 + optionCount) % optionCount;
-        }
-        else if (key == 80) { // Down arrow key
-            selected = (selected + 1) % optionCount;
-        }
-        else if (key == '\r') { // Enter key
-            system("cls");
-            if (options[selected] == "Start Game") {
-                //sselect character
-                characterSelection();
+            char key = _getch(); // Get user input without pressing Enter
+            if (key == 72) { // Up arrow key
+                selected = (selected - 1 + optionCount) % optionCount;
             }
-            else if (options[selected] == "Exit") {
-                break;
-			}
+            else if (key == 80) { // Down arrow key
+                selected = (selected + 1) % optionCount;
+            }
+            else if (key == '\r') { // Enter key
+                system("cls");
+                if (options[selected] == "Start Game") {
+                    //sselect character
+                    characterSelection();
+                }
+                else if (options[selected] == "Exit") {
+                    break;
+                }
 
-            /*cout << "You selected: " << options[selected] << endl;*/
-            /*if (options[selected] == "Exit") break;
-            system("pause");*/
+                /*cout << "You selected: " << options[selected] << endl;*/
+                /*if (options[selected] == "Exit") break;
+                system("pause");*/
+            }
         }
+
+
+        return 0;
     }
 
 
-    return 0;
-}
 
 
 
@@ -813,15 +906,13 @@ int main()
 
 
 
+    // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+    // Debug program: F5 or Debug > Start Debugging menu
 
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+    // Tips for Getting Started: 
+    //   1. Use the Solution Explorer window to add/manage files
+    //   2. Use the Team Explorer window to connect to source control
+    //   3. Use the Output window to see build output and other messages
+    //   4. Use the Error List window to view errors
+    //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
