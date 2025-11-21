d4e1dd5fd72d5ba67096739ca0fc1c67858cafc0
HughTribe <229113637+HughTribe@users.noreply.github.com>
2025-11-21 16:36:03 +0000

    decent commit considering i normally do no work in lab

--- Assessment 1 Challenge 7/Assessment 1 Challenge 7/Assessment 1 Challenge 7.cpp
+++ Assessment 1 Challenge 7/Assessment 1 Challenge 7/Assessment 1 Challenge 7.cpp
@@ -4,10 +4,40 @@
 #include <iostream>
 #include <string>
 #include <conio.h> //for _getch()
+#include <vector>
+using namespace std;	
 
-std::string PromptForAndOutputName(std::string player)
+class Weapon
 {
-		std::cout << "Enter " << player << "'s name: ";
+
+	public:
+	Weapon(const string& n, const string& iT, const int& p, const int& d, const string& r)	
+	{
+		name = n;
+		itemType = iT;
+		price = p;
+		damage = d;
+		role = r;
+	}
+	string name;
+	string itemType;
+	int price = 0;
+	int damage = 0;
+	string role;
+};
+Weapon greatSword("Great Sword", "Sword", 40, 100, "Knight");
+Weapon scimitar("Scimitar", "Sword", 35, 75, "Knight");
+Weapon dagger("Dagger", "Sword", 10, 35, "Knight");	
+Weapon longbow("Longbow", "Bow", 20, 55, "Archer");
+Weapon crossbow("Crossbow", "Bow", 40, 100, "Archer");
+Weapon rustySpear("Rusty Spear", "Spear", 10, 35, "Spearman");
+Weapon ironSpear("Iron Spear", "Spear", 20, 65, "Spearman");
+
+vector<Weapon> shopInventory = { greatSword, scimitar, dagger, longbow, crossbow, rustySpear, ironSpear };
+
+std::string PromptForAndOutputName(std::string& player)
+{
+		std::cout << "- Enter " << player << "'s name: ";
 		std::cin >> player;
 		std::cout << '\n';
 	  
@@ -25,8 +55,12 @@ int main()
 		PromptForAndOutputName(player2);
 		PromptForAndOutputName(player3);
 
-		std::cout << "welcome " << player1 << ", " << player2 << ", and " << player3 << " to the shop!\n";
-		std::cout << "You currently have 100 coins. You can buy the following items";
+		std::cout << ">> welcome " << player1 << ", " << player2 << ", and " << player3 << " to the shop!\n";
+		std::cout << "You currently have 100 coins. You can buy the following items\n";
+		for (int i = 0; i < shopInventory.size(); i++)
+		{
+			std::cout << ">> " << i + 1 <<". " << shopInventory.at(i).name << " (" << shopInventory.at(i).price << " coins, " << shopInventory.at(i).damage << " Damage)\n";
+		}
 	} while (!HappyWithSelectionOfWeapons);
 	
 }
